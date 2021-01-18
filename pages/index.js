9a8c60acafd8abae3db24cff4794cf404246427c
josuke0227 <45995961+josuke0227@users.noreply.github.com>
2021-01-19 08:21:55 +1100

    changed my name

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <selection className={utilStyles.headingMd}>
-        <p>Hello ,Im Josuke.</p>
+        <p>Hello ,Im Yosuke.</p>
         <p>
           (This is a sample website - you'll be building a site like this on )
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
